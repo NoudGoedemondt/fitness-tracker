e9b2a859ceb453c14b11484b4f8ce187a6147364
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-05-17 15:47:03 +0200

    created general calendar

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <template>
   <the-header></the-header>
-  <h3>My List</h3>
-  <ul>
-    <base-list-item>
-      <h3>Item 1</h3>
-      <p>This is the first item.</p>
-    </base-list-item>
-    <base-list-item>
-      <h3>Item 2</h3>
-      <p>This is the second item.</p>
-    </base-list-item>
-  </ul>
+  <calendar-list></calendar-list>
 </template>
 
 <script>
 import TheHeader from './components/UI/TheHeader.vue';
+import CalendarList from './components/calendar/CalendarList.vue';
 
 export default {
   components: {
     TheHeader,
+    CalendarList,
   },
   data() {
     return {};
@@ -35,20 +27,9 @@ body {
   padding: 0;
 }
 
-h1,
-h2,
-h3,
-h4,
-h5,
-h6,
-p,
-ul,
-li {
+ul {
   margin: 0;
   padding: 0;
-}
-
-ul {
   list-style: none;
 }
 </style>
---
+++ src/components/calendar/CalendarItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li>
+    <slot></slot>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'CalendarItem',
+};
+</script>
+
+<style scoped>
+li {
+  background-color: #fff;
+  border-radius: 6px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  margin: 0.5rem;
+  min-width: 20px;
+  min-height: auto;
+  text-align: center;
+}
+</style>
