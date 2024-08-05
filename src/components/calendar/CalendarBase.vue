d28a0db92efe305097cbb1a419a983a3589aff68
NoudGoedemondt <noudgoedemondt@gmail.com>
2024-08-05 19:07:24 +0200

    add weekdays

--- src/components/calendar/CalendarBase.vue
+++ src/components/calendar/CalendarBase.vue
@@ -1,9 +1,53 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div class="calendar">
+    <div class="controls">
+      {{ currentDate }}
+    </div>
+
+    <week-days />
+
+    <div class="days-grid">
+      <div class="item" v-for="day in currentMonthDays" :key="day">
+        {{ day }}
+      </div>
+    </div>
+  </div>
+
+  <h1>{{ now }}</h1>
+  <h1>{{ currentMonthDays }}</h1>
+  <h1>{{ previousMonthDays }}</h1>
+  <h1>{{ nextMonthDays }}</h1>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { computed } from 'vue';
+import dayjs from 'dayjs';
+import WeekDays from './WeekDays.vue';
 
-const msg = ref('Hello world');
+const currentDate = computed(() => dayjs().format('DD-MM-YYYY'));
+
+const now = dayjs();
+const currentMonthDays = dayjs(now).daysInMonth();
+const previousMonthDays = dayjs(now).subtract(1, 'month').daysInMonth();
+const nextMonthDays = dayjs(now).add(1, 'month').daysInMonth();
 </script>
+
+<style scoped>
+.calendar {
+  max-width: 960px;
+  margin: 50px auto;
+}
+
+.days-grid {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+  gap: 1px;
+}
+
+.item {
+  border: 1px solid var(--grey-300);
+  border-radius: 4px;
+  padding: 1rem;
+  text-align: center;
+}
+</style>
---
+++ src/components/calendar/WeekDays.vue
@@ -0,0 +1,16 @@
+<template>
+  <ol class="weekdays">
+    <li v-for="day in weekdays" :key="day">{{ day }}</li>
+  </ol>
+</template>
+
+<script setup>
+const weekdays = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
+</script>
+
+<style scoped>
+.weekdays {
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+}
+</style>
