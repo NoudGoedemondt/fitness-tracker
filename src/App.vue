1cf05a6ff64c1503a7a4ea5732ff3c0b39cc516b
Noud Goedemondt <noudgoedemondt@gmail.com>
2024-09-04 16:35:09 +0200

    add style tweaks

--- src/App.vue
+++ src/App.vue
@@ -11,13 +11,17 @@ import ViewWorkout from './components/wo-manager/ViewWorkout.vue';
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 
 body {
-  font-family: sans-serif;
-  font-weight: 100;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 300;
   --grey-100: #e4e9f0;
   --grey-200: #cfd7e3;
   --grey-300: #b5c0cd;
